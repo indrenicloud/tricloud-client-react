8b7bce610bf5ddcb1331cf16870b09190de4fe40
bornjre <bornjre@gmail.com>
2019-05-09 09:32:42 +0545

    added table to taskmanager

--- src/components/TaskManager/TaskManager.jsx
+++ src/components/TaskManager/TaskManager.jsx
@@ -1,4 +1,15 @@
 import React, { Component } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+let titles = ["PID", "USER", "CPU", "MEM", "UpTime", "Command"];
 
 export default class TaskManager extends Component {
   constructor(props) {
@@ -6,20 +17,53 @@ export default class TaskManager extends Component {
     this.setState({
       termdata: {}
     });
+    this.dataloaded = false;
   }
 
   updateTerminal = data => {
     console.log(data);
+    this.dataloaded = true;
     this.setState({
-      termdata: data
+      termdata: data.Processes
     });
   };
 
   render() {
-    let content = "Terminal not running";
-    if (this.state != null) {
-      content = this.state.termdata;
-    }
-    return <div>{JSON.stringify(content)}</div>;
+    return (
+      this.dataloaded && (
+        <Table responsive>
+          <thead className="text-primary">
+            <tr>
+              {titles.map((title, key) => {
+                if (key === titles.length - 1)
+                  return (
+                    <th key={key} className="text-right">
+                      {title}
+                    </th>
+                  );
+                return <th key={key}>{title}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.termdata.map((item, key) => {
+              return (
+                <tr key={key}>
+                  {titles.map((title, key) => {
+                    if (key === titles.length - 1)
+                      return (
+                        <th key={key} className="text-right">
+                          {item[title]}
+                        </th>
+                      );
+                    return <th key={key}>{item[title]}</th>;
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )
+    );
   }
 }
