feec1c6dc62f3bdabc93918e8745017a1d7f1be8
Arun Kumar Pyasi <expertaruncorp@gmail.com>
2019-06-04 02:28:59 +0545

    added API_URL to .env

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -5,36 +5,36 @@ import Settings from "views/Settings/Settings.jsx";
 import Logout from "../components/Logout/Logout";
 
 var dashRoutes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: "nc-icon nc-bank",
-    component: Dashboard
-  },
+  // {
+  //   path: "/dashboard",
+  //   name: "Dashboard",
+  //   icon: "nc-icon nc-bank",
+  //   component: Dashboard
+  // },
   {
     path: "/agents",
     name: "Agents",
     icon: "nc-icon nc-app",
     component: Agents
   },
-  {
-    path: "/settings",
-    name: "Settings",
-    icon: "nc-icon nc-settings-gear-65",
-    component: Settings
-  },
-  {
-    path: "/user-page",
-    name: "Profile",
-    icon: "nc-icon nc-single-02",
-    component: UserPage
-  },
+  // {
+  //   path: "/settings",
+  //   name: "Settings",
+  //   icon: "nc-icon nc-settings-gear-65",
+  //   component: Settings
+  // },
+  // {
+  //   path: "/user-page",
+  //   name: "Profile",
+  //   icon: "nc-icon nc-single-02",
+  //   component: UserPage
+  // },
   {
     path: "/logout",
     name: "Logout",
     icon: "nc-icon nc-caps-small",
     component: Logout
   },
-  { redirect: true, path: "/", pathTo: "/dashboard", name: "Dashboard" }
+  { redirect: true, path: "/", pathTo: "/agents", name: "Dashboard" }
 ];
 export default dashRoutes;
